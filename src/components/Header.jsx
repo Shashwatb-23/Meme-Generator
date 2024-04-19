32bb2fbb314d7dcb0ba3bd7a84356853139343b3
Shashwat Bhardwaj <shashwatbhardwaj3@gmail.com>
2024-04-19 15:40:45 +0530

    Making a react project:- Meme Genrator

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return (
+        <div className="head">
+            <img src="public/images/Troll Face.png" alt="troll-face" className="logo" />
+            <h3>Meme Generator</h3>
+        </div>
+    )
+}
