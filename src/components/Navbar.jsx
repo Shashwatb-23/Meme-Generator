fb5aa6f0e6646c2ef7e5ce26d34a023f66ca5506
Shashwat Bhardwaj <shashwatbhardwaj3@gmail.com>
2024-04-19 20:23:54 +0530

    Completed with the navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,19 @@
 export default function Navbar(){
     return(
             <div className="navbar">
-                <div className="input">
-                <label>Top text</label><br />
-                <input type="text" maxLength={50} placeholder="Top text"/>
-                <label>Bottom text</label><br />
-                <input type="text" maxLength={50} placeholder="Bottom text"/>
+                <div className="input-field">
+                    <div className="top-text">
+                        <h4>Top text: </h4>
+                        <input type="text" maxLength={50} placeholder="Top text"/>
+                    </div>
+                    <div className="bottom-text">
+                        <h4>Bottom text: </h4>
+                        <input type="text" maxLength={50} placeholder="Bottom text"/>
+                    </div>
                 </div>
-                <button className="btn">Get a new meme image <img src="public/images/frame.jfif" alt="" className="frame"/></button>
+                    <div className="generate-btn">
+                        <button className="btn">Get a new meme image <img src="public/images/frame.jfif" alt="" className="frame"/></button>
+                    </div>
             </div>
         )
 }
