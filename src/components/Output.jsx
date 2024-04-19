dbf28568812e4dbbfe9b4e050ac84a35b8eb1cd9
Shashwat Bhardwaj <shashwatbhardwaj3@gmail.com>
2024-04-19 20:31:17 +0530

    halfway through the Output compnent

---
+++ src/components/Output.jsx
@@ -0,0 +1,7 @@
+export default function Output (){
+    return(
+        <div className="output">
+            <img src="public/images/image-shut-up-and -take...jfif" alt="coverImg" className="coverimg" />
+        </div>
+    )
+}
